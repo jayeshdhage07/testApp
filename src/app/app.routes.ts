d1f3dede73cb4f6a33fbfb25d90e689c1a4c6a56
amjayesh07 <jshdhage@gmail.com>
2024-08-31 11:47:20 +0530

    import bootstrap, routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent, },
+    { path: '', redirectTo:'home', pathMatch:'full'},
+    { path: 'about', component: AboutComponent, },
+    { path: 'product', component: ProductComponent, },
+    { path: 'contact', component: ContactComponent, },
+    { path: '**', component: PagenotfoundComponent, }
+];
