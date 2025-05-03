c4925fa6a2fb70f2a815b9d1070244d2656cee4b
jayeshdhage07 <jshdhage@gmail.com>
2025-05-03 18:16:22 +0530

    few changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CommonModule } from '@angular/common';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, RouterTestingModule, CommonModule],
     }).compileComponents();
   });
 
